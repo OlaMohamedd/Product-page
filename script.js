163e7d0a897b7a3c8343bc36826452909a952f4c
OlaMohamedd <olagemia@gmail.com>
2023-10-29 12:50:25 +0200

    Increment and Decrement Button

---
+++ script.js
@@ -0,0 +1,50 @@
+// Get references to the buttons and value display
+const incrementBtn = document.getElementById("increment");
+const decrementBtn = document.getElementById("decrement");
+const valueDisplay = document.getElementById("value");
+
+// Set an initial value
+let value = 0;
+
+// Add event listeners to the buttons
+incrementBtn.addEventListener("click", () => {
+  value++;
+  valueDisplay.textContent = value;
+});
+
+decrementBtn.addEventListener("click", () => {
+  value--;
+  valueDisplay.textContent = value;
+});
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
